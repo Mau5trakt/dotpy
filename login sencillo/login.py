cf27a01a23b6b9ceaee81776f2208b6f4c636967
Mau5trakt <kubzalemany@hotmail.com>
2021-05-17 16:47:38 +0000

    updated login, read the description

--- login sencillo/login.py
+++ login sencillo/login.py
@@ -1,5 +1,5 @@
 print("Bienvenido al sistema, ingrese sus credenciales a continuación")
-login = {84289860}
+login = [84289860]
 op = ""
 while op not in ['R', 'I']:
     op = input('Registrarse o Iniciar sesion[R/I]: ')
@@ -10,16 +10,30 @@ while op not in ['R', 'I']:
             surname = input("Ingrese su apellido: ")
             num = input("Ingrese su número de telefono: ")
             confir = input(f"¿Su nombre es {name} {surname} y su número telefónico es {num}?[S/N]:")
+            
+
             '''
             Bueno aqui es donde conecto la db y le paso los valores que salen en confir
             '''
-        print("Usted se ha registrado satisfactoriamente")
+        num = int(num)    
+        if num in login: 
+            print("Usted ya se encuentra registrado en el sistema \n por favor inicie sesion")
+        elif num not in login:
+            print("Usted se ha registrado satisfactoriamente")
+            login.append(num)
+            print(login)
     elif op == 'I':
         conf = ""
         while conf not in login:
             conf = int(input("Ingrese su número telefónico"))
             if conf in login:
                 print("Bienvenido")
+                ''' 
+                Bueno mae aqui va lo del ingreso.py nada mas
+                '''
+            else: 
+                print("id incorrecto o no existente en el sistema")
+        
 
 ''''#aqui cargo la db y segun el numero obtengo el nombre
 Bueno la verdad es que voy a hacerlo con el username y pass pero es de prueba para ver como funciona xD
