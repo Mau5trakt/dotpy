0e821f917f54e891a0def03e5f722a43cca7be70
Mau5trakt <80871251+Mau5trakt@users.noreply.github.com>
2021-06-17 15:27:39 -0600

    random generator

--- login sencillo/pruebapfor.py
+++ login sencillo/pruebapfor.py
@@ -1,4 +1,9 @@
 import random
+numeritos = []
 for aleatnum in range (11):
     aleatnum = [random.randint(1,15), random.randint(1,15)]
+    numeritos.append(aleatnum)
     print(aleatnum)
+
+
+print(f"a{numeritos}")
